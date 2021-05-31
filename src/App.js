4fd4963c38a7bfedc8699de1e7ec042b3750eecf
Tyler Hawkins <thawkin3@byu.net>
2021-05-31 09:58:35 -0600

    chore: initializes the app, packages, and README

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>TestProject JavaScript OpenSDK Demo</h1>
       </header>
-    </div>
-  );
+    </main>
+  )
 }
-
-export default App;
