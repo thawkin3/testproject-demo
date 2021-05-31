cfa031037d5663ee6b5f58eab7c9c49031e05f66
Tyler Hawkins <thawkin3@byu.net>
2021-05-31 11:38:33 -0600

    feat: adds intro text to the app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ export const App = () => {
   return (
     <main>
       <h1>TestProject JavaScript OpenSDK Demo</h1>
+      <div className="introduction">
+        <p>
+          Let's pretend that the form below is used to request a product demo.
+        </p>
+        <p>
+          You can find the{' '}
+          <a href="https://github.com/thawkin3/testproject-demo">
+            source code on GitHub
+          </a>{' '}
+          to view the end-to-end tests written for this app using the{' '}
+          <a href="https://github.com/testproject-io/javascript-opensdk">
+            TestProject JavaScript OpenSDK
+          </a>
+          .
+        </p>
+      </div>
       <RequestForm />
     </main>
   )
