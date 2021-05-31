a7f3b3f8a2371d6336d734da418532ddcbdcf5de
Tyler Hawkins <thawkin3@byu.net>
2021-05-31 12:15:28 -0600

    refactor: uses beforeEach and afterEach blocks in e2e test file

--- src/App.e2e.test.js
+++ src/App.e2e.test.js
@@ -2,10 +2,20 @@ import { By } from 'selenium-webdriver'
 import { Builder } from '@tpio/javascript-opensdk'
 
 describe('App', () => {
-  it('allows the user to submit the form when filled out properly', async () => {
-    const driver = await new Builder().forBrowser('chrome').build()
+  const testUrl = 'http://localhost:3000/testproject-demo/build/'
 
-    await driver.get('http://localhost:3000/testproject-demo/build/')
+  let driver
+
+  beforeEach(async () => {
+    driver = await new Builder().forBrowser('chrome').build()
+  })
+
+  afterEach(async () => {
+    await driver.quit()
+  })
+
+  it('allows the user to submit the form when filled out properly', async () => {
+    await driver.get(testUrl)
     await driver.findElement(By.css('#firstName')).sendKeys('John')
     await driver.findElement(By.css('#lastName')).sendKeys('Doe')
     await driver.findElement(By.css('#email')).sendKeys('john.doe@email.com')
@@ -14,20 +24,14 @@ describe('App', () => {
     await driver
       .findElement(By.css('#submissionConfirmationText'))
       .isDisplayed()
-
-    await driver.quit()
   })
 
   it('prevents the user from submitting the form when not filled out properly', async () => {
-    const driver = await new Builder().forBrowser('chrome').build()
-
-    await driver.get('http://localhost:3000/testproject-demo/build/')
+    await driver.get(testUrl)
     await driver.findElement(By.css('#requestDemo')).click()
 
     await driver.findElement(By.css('#firstNameError')).isDisplayed()
     await driver.findElement(By.css('#lastNameError')).isDisplayed()
     await driver.findElement(By.css('#emailError')).isDisplayed()
-
-    await driver.quit()
   })
 })
